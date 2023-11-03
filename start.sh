62b04822278455af4fe13cc831016800a836f4fc
Mrcnu <134508260+Mrcnu@users.noreply.github.com>
2023-11-03 18:34:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Mrcnu/adityaDQbot.git /DQTheFileDonorBot
+  git clone https://github.com/Mrcnu/naadqmovies /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQTheFileDonorBot
